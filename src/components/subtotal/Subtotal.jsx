db9bbecaff0ca1c63b0e2d569f2a8baf5fa7fb04
kartik yadav <kartiky211099@gmail.com>
2025-09-03 14:31:14 +0530

    Update: add packagge.json

--- src/components/subtotal/Subtotal.jsx
+++ src/components/subtotal/Subtotal.jsx
@@ -1,39 +1,43 @@
-import "./Subtotal.css"
-import React from 'react'
-import CurrencyFormat from "react-currency-format";
+import "./Subtotal.css";
+import React from "react";
+import CurrencyInput from "react-currency-input-field";
 import { useStateValue } from "../../StateProvider";
 import { getBasketTotal } from "../../reducer";
 
 const Subtotal = () => {
-    const [{ basket }] = useStateValue();
+  const [{ basket }] = useStateValue();
 
   return (
     <div className="subtotal">
+      {/* Display subtotal manually */}
+      <p>
+        Subtotal ({basket.length}):{" "}
+        <strong>
+          <CurrencyInput
+            value={getBasketTotal(basket)}
+            decimalsLimit={2}
+            prefix="$"
+            readOnly
+          />
+        </strong>
+      </p>
 
-        <CurrencyFormat
-        renderText={(value) => (
-            <>
-            <p>
-                Subtotal ({basket.length}): <strong>{value}</strong>
-            </p>
-            <small className="subtotal__gift">
-                <input type="checkbox" /> This order contains a gift
-            </small>
-            </>
-        )}
+      <small className="subtotal__gift">
+        <input type="checkbox" /> 
+        This order contains a gift
+      </small>
 
-        decimalScale={2}
-        value={getBasketTotal(basket)}
-        fixedDecimalScale={true}
-        displayType={"text"}
-        thousandSeparator={true}
-        prefix={"$"}
-        />
-        <button >Processed to checkout</button>
-
-        
+      <button>Proceed to checkout</button>
     </div>
-  )
-}
+  );
+};
 
-export default Subtotal
+export default Subtotal;
+
+
+
+
+
+
+
+
